a9a6424219244f4dbe73e642c23762bc68a969a2
Leo <lsanchez.ve@gmail.com>
2022-11-30 10:05:51 -0500

    Initial commit

---
+++ src/processor.ts
@@ -0,0 +1,37 @@
+import { TypeormDatabase } from "@subsquid/typeorm-store";
+import {EvmBatchProcessor} from '@subsquid/evm-processor'
+
+const processor = new EvmBatchProcessor()
+  .setDataSource({
+    // uncomment and set ETHEREUM_MAINNET_WSS to 
+    // a websocket JSON-RPC Etheruem mainnet endpoint in
+    // order to enable contract state queries:
+    // chain: process.env.ETHEREUM_MAINNET_WSS,
+    archive: 'https://eth.archive.subsquid.io',
+  })
+  .addTransaction([
+    '0x0000000000000000000000000000000000000000'
+  ], {
+    range: {
+      from: 10_000_000
+    },
+    data: {
+      transaction: {
+        from: true,
+        input: true,
+        to: true
+      }
+    }
+  });
+
+
+processor.run(new TypeormDatabase(), async (ctx) => {
+  for (let c of ctx.blocks) {
+    for (let i of c.items) {
+      // apply arbitrary data transformation logic here
+      // use ctx.store to persist the data
+      ctx.log.info(i, "Next item:")
+    }
+  }
+});
+
